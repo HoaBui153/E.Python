0802cd4715e981fab6635fd6884e57ff9cee6137
HoaBui <hoa.bui153@gmail.com>
2018-03-29 20:28:59 +0700

    first commit

---
+++ #4 - Data Type - String - Number.py
@@ -0,0 +1,15 @@
+import os
+os.system('cls')
+#1+2
+#2**2
+str= 'string'
+print(str[0])
+print(str[1:])
+numbers = [1,2,3,4,5]
+numbers[1] + numbers[2]
+for i in numbers:
+	print(i)
+numbers = {'Bob':'322','Mary':'110', 'Joe':'222'}
+print(numbers['Bob'])
+poss = 'Mike''s'
+print(poss)
